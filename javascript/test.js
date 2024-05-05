8f96d32a52b68c27fa41da0a20ac4d78ea3105d4
Suraj Tudu <surajtudu3007@gmail.com>
2024-05-05 08:56:07 +0000

    Testing Javascript

--- javascript/test.js
+++ javascript/test.js
@@ -1,1 +1,54 @@
 console.log('Suraj Tudu')
+const accId = 56784
+let emailId = "tempo@gmail.com"
+let location = "Gurugram"
+
+// console.table([accId, emailId, location])
+
+console.log(`Account ID : ${accId}`)
+// console.log(emailId)
+// console.log(location)
+
+// console.log(typeof accId)
+// let con = String(accId)
+// console.log(typeof con)
+
+// Dates and Time
+
+let date = new Date()
+// console.log(Date.now())
+// console.log(date)
+// console.log(date.toLocaleString('default', {
+//     weekday: "long",}))
+// console.log(date)
+
+
+// Array and Object
+
+let arr = [23,76,'Momo ',false,'Excercise ']
+let arrTwo = [54,89,23,'Hitesh','Kohinaa ']
+// let obj = {
+//     name : 'Object',
+//     age : 22,
+//     isloggedIn : true
+// }
+
+// console.log(arr)
+// console.log(obj)
+
+// Revision Required
+
+// let newarr=arr
+// arrTwo.join(newarr)
+// console.log(arrTwo.join(newarr))
+
+// console.log(Array.isArray("Hitesh"))
+// console.log(Array.from("Hitesh"))
+// console.log(Array.from({name: "hitesh"})) // interesting
+
+
+
+
+
+
+
